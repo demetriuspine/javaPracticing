96dac1ae8f07cad0adc01b9988d878bccb7c70e4
lucas <mr.pine97@gmail.com>
2021-08-04 15:12:05 -0300

    declaração de variáveis

---
+++ src/variables.java
@@ -0,0 +1,19 @@
+public class variables {
+  public static void main(String[] args) {
+    //declarando variáveis no java
+    String nome = "zé";//string
+    int idade = 10;//inteiro
+    short nota = 7;//inteiro, mas usa menos espaço na memória
+    double altura = 1.98;//float, mas ocupa menos memória
+    float peso = 99.7f;//float, mas precisa do f e ocupa mais memoria
+    boolean oculos = true;
+
+    System.out.println("nome: " + nome);
+    System.out.println("idade: " + idade);
+    System.out.println("nota: " + nota);
+    System.out.println("altura: " + altura);
+    System.out.println("peso: " + peso);
+    System.out.println("Óculos " + oculos);
+
+  }
+}
