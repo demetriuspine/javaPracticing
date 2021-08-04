ee488266f3885f41abdf4dca3cfc71d4324be819
lucas <mr.pine97@gmail.com>
2021-08-04 15:08:58 -0300

    renomeação

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-  public static void main(String[] args) {
-    System.out.print("Olá, mundo!");//deixa tudo em uma linha
-    System.out.print("Olá camarada");
-    System.out.println("mensagem");//pula uma linha
-    System.out.print("outra mensagem");
-    System.out.println("mais outra mensagem");
-  }
-}
