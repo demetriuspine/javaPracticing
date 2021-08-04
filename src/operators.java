e3afa882c7e69bd38ae8268ad1dd5bf5f343790f
lucas <mr.pine97@gmail.com>
2021-08-04 14:56:25 -0300

    operadores

---
+++ src/operators.java
@@ -0,0 +1,16 @@
+public class operators {
+  public static void main(String[] args) {
+    int x=9 ,y=10 ,z=15;
+
+    float k=10.5f, n=12.7f;
+
+    System.out.println("x * y = " + (x*y));
+    System.out.println("x / y = " + (x/y));
+    System.out.println("x - y = " + (x-y));
+    System.out.println("x + y = " + (x+y));
+    System.out.println("y % z = " + (y%z));
+    System.out.println((5f+2)/2);//se nao colocar o f, ele deixa inteiro
+    System.out.println("k + n = " + (k+n));
+    System.out.println("k * n = " + (k*n));
+  }
+}
