ea3ab3bd3127ce2c7ff55a9ef3153a0b51b551bb
lucas <mr.pine97@gmail.com>
2021-08-04 14:53:49 -0300

    sintaxe do output

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.print("Olá, mundo!");//deixa tudo em uma linha
+    System.out.print("Olá camarada");
+    System.out.println("mensagem");//pula uma linha
+    System.out.print("outra mensagem");
+    System.out.println("mais outra mensagem");
+  }
 }
